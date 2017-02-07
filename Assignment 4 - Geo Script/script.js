9389954af4225ea08a85ea1a0270f1698562ef0e
Camille Sébastien Niessen <camille.s.niessen@icloud.com>
2017-02-07 14:47:37 +0100

    Finished: Assignment 4 - Geo script refactoring

--- Assignment 4 - Geo Script/script.js
+++ Assignment 4 - Geo Script/script.js
@@ -5,6 +5,37 @@
 // Makes sure that variables aren't global
 (function() {
 
+  function EventTarget() {
+      this._listeners = {}
+  }
+  EventTarget.prototype = {
+      constructor: EventTarget,
+      addListener: function(a, c) {
+          "undefined" == typeof this._listeners[a] && (this._listeners[a] = []);
+          this._listeners[a].push(c)
+      },
+      fire: function(a) {
+          "string" == typeof a && (a = {
+              type: a
+          });
+          a.target || (a.target = this);
+          if (!a.type) throw Error("Event object missing 'type' property.");
+          if (this._listeners[a.type] instanceof Array)
+              for (var c = this._listeners[a.type], b = 0, d = c.length; b < d; b++) c[b].call(this, a)
+      },
+      removeListener: function(a, c) {
+          if (this._listeners[a] instanceof Array)
+              for (var b =
+                      this._listeners[a], d = 0, e = b.length; d < e; d++)
+                  if (b[d] === c) {
+                      b.splice(d, 1);
+                      break
+                  }
+      }
+  };
+  
+  var ET = new EventTarget();
+
   // Array with all constant variables, stored in an array.
   var varArray = {
 
@@ -133,8 +164,82 @@
 
        var routeList = [];
        // Add the markers to themap dependending on the tour type
-       
+       debug_message("Locaties intekenen, tourtype is: " + tourType);
+       for (var i = 0; i < locaties.length; i++) {
+         try {
+             (localStorage.visited == undefined || isNumber(localStorage.visited)) ? localStorage[locaties[i][0]] = false : null;
+         } catch (error) {
+             debug_message("Localstorage can't be reached: " + error);
+         }
+         var markerLatLng = new google.maps.LatLng(locaties[i][3], locaties[i][4]);
+         routeList.push(markerLatLng);
 
+         varArray.markerArray[i] = {};
+         for (var attr in locatieMarker) {
+             varArray.markerArray[i][attr] = locatieMarker[attr];
+         }
+         markerArray[i].scale = locaties[i][2] / 3;
+
+         var marker = new google.maps.Marker({
+             position: markerLatLng,
+             map: varArray.map,
+             icon: varArray.markerArray[i],
+             title: locaties[i][0]
+         });
+       }
+
+       if(tourType == varArray.lineair){
+           // Draw lines between the points
+           debug_message("Drawing route");
+           var route = new google.maps.Polyline({
+               clickable: false,
+               map: varArray.map,
+               path: routeList,
+               strokeColor: 'Black',
+               strokeOpacity: .6,
+               strokeWeight: 3
+           });
+       }
+
+       // Add the location of the user
+       varArray.currentPositionMarker = new google.maps.Marker({
+           position: kaartOpties.center,
+           map: varArray.map,
+           icon: positieMarker,
+           title: 'U are here'
+       });
+
+       ET.addListener(varArray.positionUpdated, update_positie);
+
+     },
+
+     isNumber: function(n) {
+       return !isNaN(parseFloat(n)) && isFinite(n);
+     },
+
+     // Update the users position on the map
+     update_positie: function(event) {
+       // use currentPosition to center the map
+       var newPos = new google.maps.LatLng(varArray.currentPosition.coords.latitude, varArray.currentPosition.coords.longitude);
+       varArray.map.setCenter(newPos);
+       varArray.currentPositionMarker.setPosition(newPos);
+     }
+
+   };
+
+   var debugModule = {
+
+     _geo_error_handler: function(code, message) {
+       debug_message('geo.js error ' + code + ': ' + message);
+     },
+
+     debug_message: function(message) {
+       (varArray.customDebugging && varArray.debugId) ? document.getElementById(varArray.debugId).innerHTML : console.log(message);
+     },
+
+     set_custom_debugging: function(debugId) {
+       varArray.debugId = this.debugId;
+       varArray.customDebugging = true;
      }
 
    };
@@ -151,96 +256,3 @@ https://github.com/ju5tu5/cmdgeo/blob/master/src/cmdgeo-0.1.js
 
 Code refactoring: Camille Sébastien Niessen
 */
-
-
-
-
-function generate_map(myOptions, canvasId){
-
-    var routeList = [];
-    // Voeg de markers toe aan de map afhankelijk van het tourtype
-    debug_message("Locaties intekenen, tourtype is: "+tourType);
-    for (var i = 0; i < locaties.length; i++) {
-
-        // Met kudos aan Tomas Harkema, probeer local storage, als het bestaat, voeg de locaties toe
-        try {
-            (localStorage.visited==undefined||isNumber(localStorage.visited))?localStorage[locaties[i][0]]=false:null;
-        } catch (error) {
-            debug_message("Localstorage kan niet aangesproken worden: "+error);
-        }
-
-        var markerLatLng = new google.maps.LatLng(locaties[i][3], locaties[i][4]);
-        routeList.push(markerLatLng);
-
-        markerRij[i] = {};
-        for (var attr in locatieMarker) {
-            markerRij[i][attr] = locatieMarker[attr];
-        }
-        markerRij[i].scale = locaties[i][2]/3;
-
-        var marker = new google.maps.Marker({
-            position: markerLatLng,
-            map: map,
-            icon: markerRij[i],
-            title: locaties[i][0]
-        });
-    }
-// TODO: Kleur aanpassen op het huidige punt van de tour
-    if(tourType == LINEAIR){
-        // Trek lijnen tussen de punten
-        debug_message("Route intekenen");
-        var route = new google.maps.Polyline({
-            clickable: false,
-            map: map,
-            path: routeList,
-            strokeColor: 'Black',
-            strokeOpacity: .6,
-            strokeWeight: 3
-        });
-
-    }
-
-    // Voeg de locatie van de persoon door
-    currentPositionMarker = new google.maps.Marker({
-        position: kaartOpties.center,
-        map: map,
-        icon: positieMarker,
-        title: 'U bevindt zich hier'
-    });
-
-    // Zorg dat de kaart geupdated wordt als het POSITION_UPDATED event afgevuurd wordt
-    ET.addListener(POSITION_UPDATED, update_positie);
-}
-
-function isNumber(n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
-}
-
-// Update de positie van de gebruiker op de kaart
-function update_positie(event){
-    // use currentPosition to center the map
-    var newPos = new google.maps.LatLng(currentPosition.coords.latitude, currentPosition.coords.longitude);
-    map.setCenter(newPos);
-    currentPositionMarker.setPosition(newPos);
-}
-
-// FUNCTIES VOOR DEBUGGING
-
-function _geo_error_handler(code, message) {
-    debug_message('geo.js error '+code+': '+message);
-}
-function debug_message(message){
-    (customDebugging && debugId)?document.getElementById(debugId).innerHTML:console.log(message);
-}
-function set_custom_debugging(debugId){
-    debugId = this.debugId;
-    customDebugging = true;
-}
-
-
-
-// Event functies - bron: http://www.nczonline.net/blog/2010/03/09/custom-events-in-javascript/ Copyright (c) 2010 Nicholas C. Zakas. All rights reserved. MIT License
-// Gebruik: ET.addListener('foo', handleEvent); ET.fire('event_name'); ET.removeListener('foo', handleEvent);
-function EventTarget(){this._listeners={}}
-EventTarget.prototype={constructor:EventTarget,addListener:function(a,c){"undefined"==typeof this._listeners[a]&&(this._listeners[a]=[]);this._listeners[a].push(c)},fire:function(a){"string"==typeof a&&(a={type:a});a.target||(a.target=this);if(!a.type)throw Error("Event object missing 'type' property.");if(this._listeners[a.type]instanceof Array)for(var c=this._listeners[a.type],b=0,d=c.length;b<d;b++)c[b].call(this,a)},removeListener:function(a,c){if(this._listeners[a]instanceof Array)for(var b=
-this._listeners[a],d=0,e=b.length;d<e;d++)if(b[d]===c){b.splice(d,1);break}}}; var ET = new EventTarget();
