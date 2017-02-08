bbaea40294cd0f521ff438f0a7c6a5227a393585
Camille Sébastien Niessen <camille.s.niessen@icloud.com>
2017-02-08 14:08:08 +0100

    Add: Assignment 5, Single webpage setup

---
+++ Assignment 5 - Spa/assets/js/app.js
@@ -0,0 +1,47 @@
+(function() {
+  "use strict";
+
+  var app = {
+    init: function() {
+      routes.init();
+    }
+  };
+
+  var routes = {
+    init: function() {
+      var location = window.location;
+      // Object that stores all location data
+      var oldHash = location.hash;
+      var newHash = oldHash;
+      if(!newHash) {
+        newHash = "#introduction";
+      }
+      // Check for changes in hash, then run this function
+      window.onhashchange = function() {
+        oldHash = newHash;
+        newHash = location.hash;
+        var route = {
+          old: oldHash,
+          new: newHash
+        };
+        // Send hash data to the sections toggle function.
+        sections.toggle(route);
+      }
+    }
+  };
+
+  var sections = {
+    toggle: function(route) {
+      // Toggle old hash to display none, the new one to display block
+      var oldView = document.querySelector(route.old);
+      var newView = document.querySelector(route.new);
+      oldView.classList.add('hidden');
+      newView.classList.remove('hidden');
+    }
+  };
+
+  // Start app.init function
+  app.init();
+
+  }()
+);
