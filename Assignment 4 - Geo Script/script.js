e8c297b0fd3bb9550c8ea92d997d44146e0bccde
Camille Sébastien Niessen <camille.s.niessen@icloud.com>
2017-02-08 12:10:47 +0100

    Edit: Feedback from lesson processed

--- Assignment 4 - Geo Script/script.js
+++ Assignment 4 - Geo Script/script.js
@@ -33,12 +33,11 @@
                   }
       }
   };
-  
+
   var ET = new EventTarget();
 
   // Array with all constant variables, stored in an array.
-  var varArray = {
-
+  var config = {
     sandbox: "SANDBOX",
     lineair: "LINEAIR",
     gpsAvailable: "GPS_AVAILABLE",
@@ -55,55 +54,43 @@
     updateMap: false,
     locationArray: [],
     markerArray: []
-
   }
 
   var locationModule = {
-
     // Test for GPS availability (in geo.js) and fire a event.
     init: function() {
-
-      ET.addListener(varArray.gpsAvailable, _start_interval);
-      ET.addListener(varArray.gpsUnavailable, function() {
-        debug_message('GPS is not available.')
+      ET.addListener(config.gpsAvailable, startInterval);
+      ET.addListener(config.gpsUnavailable, function() {
+        debugMessage('GPS is not available.')
       });
-
-      (geo_position_js.init()) ? ET.fire(varArray.gpsAvailable) : ET.fire(varArray.gpsUnavailable);
-
+      (geo_position_js.init()) ? ET.fire(config.gpsAvailable) : ET.fire(config.gpsUnavailable);
     },
 
     // Start interval which based on the refreshRate, updates the position
-    _start_interval: function(event) {
-
-      debug_message("GPS is available, asking for position.");
-      _update_position();
-      varArray.interval = self.setInterval(_update_position, varArray.refreshRate);
-      ET.addListener(varArray.positionUpdated, _check_locations);
-
+    startInterval: function(event) {
+      debugMessage("GPS is available, asking for position.");
+      updatePosition();
+      config.interval = self.setInterval(updatePosition, config.refreshRate);
+      ET.addListener(config.positionUpdated, checkLocations);
     },
 
     // Ask for the actual position in geo.js, initiate a callback for the result
-    _update_position: function() {
-
-      varArray.intervalCounter++;
-      geo_position_js.getCurrentPosition(_set_position, _geo_error_handler, {
+    updatePosition: function() {
+      config.intervalCounter++;
+      geo_position_js.getCurrentPosition(setPosition, geoErrorHandler, {
         enableHighAccuracy: true
       });
-
     },
 
     // Callback function for initializing the actual position, fires a event.
-    _set_position: function(position) {
-
-      varArray.currentPosition = position
-      ET.fire("varArray.positionUpdated");
-      debug_message(varArray.intervalCounter + " position lat: " + position.coords.latitude + " long: " + position.coords.longitude);
-
+    setPosition: function(position) {
+      config.currentPosition = position
+      ET.fire("config.positionUpdated");
+      debugMessage(config.intervalCounter + " position lat: " + position.coords.latitude + " long: " + position.coords.longitude);
     },
 
     // Check the location and redirect to a different page when on location
-    _check_locations: function(event) {
-
+    checkLocations: function(event) {
       for (var i = 0; i < locaties.length; i++) {
           var locatie = {
             coords: {
@@ -111,40 +98,35 @@
               longitude: locaties[i][4]
             }
           };
-
-          if(_calculate_distance(locatie, varArray.currentPosition) < locaties[i][2]) {
+          if(calculateDistance(locatie, config.currentPosition) < locaties[i][2]) {
               // Check if we are on that location, if not were going to the page
               if(window.location != locaties[i][1] && localStorage[locaties[i][0]] == "false") {
                   // Try local storage, if it exists increment the location
                   try {
                       (localStorage[locaties[i][0]] == "false") ? localStorage[locaties[i][0]] = 1 : localStorage[locaties[i][0]]++;
                   } catch(error) {
-                      debug_message("Localstorage cant be reached: " + error);
+                      debugMessage("Localstorage cant be reached: " + error);
                   }
                   // TODO: Animate the actual marker
                   window.location = locaties[i][1];
-                  debug_message("Player is within a range of " + locaties[i][2] + " meter of " + locaties[i][0]);
-
+                  debugMessage("Player is within a range of " + locaties[i][2] + " meter of " + locaties[i][0]);
                 }
               }
             }
-
       },
 
       // Calculate difference between two points in meters
-      _calculate_distance: function(p1, p2) {
-
+      calculateDistance: function(p1, p2) {
         var pos1 = new google.maps.LatLng(p1.coords.latitude, p1.coords.longitude);
         var pos2 = new google.maps.LatLng(p2.coords.latitude, p2.coords.longitude);
         return Math.round(google.maps.geometry.spherical.computeDistanceBetween(pos1, pos2), 0);
-
       }
     };
 
 
   /** GOOGLE MAPS FUNCTIONS
       ---------------------
-       * generate_map(myOptions, canvasId)
+       * generateMap(myOptions, canvasId)
        * calls based on given options the google maps API
        * to generate a map and places this in the HTML element
        * thats indicated by the given ID
@@ -156,61 +138,57 @@
        */
 
    var gMapsModule = {
-
-     generate_map: function(myOptions, canvasID) {
+     generateMap: function(myOptions, canvasID) {
        // TODO: Can I call the maps API async? Less calls
-       debug_message("Generate a Google Maps maps and show this in #" + canvasId);
-       varArray.map = new google.maps.Map(document.getElementById(canvasId), myOptions);
+       debugMessage("Generate a Google Maps maps and show this in #" + canvasId);
+       config.map = new google.maps.Map(document.getElementById(canvasId), myOptions);
 
        var routeList = [];
        // Add the markers to themap dependending on the tour type
-       debug_message("Locaties intekenen, tourtype is: " + tourType);
+       debugMessage("Locaties intekenen, tourtype is: " + tourType);
        for (var i = 0; i < locaties.length; i++) {
          try {
              (localStorage.visited == undefined || isNumber(localStorage.visited)) ? localStorage[locaties[i][0]] = false : null;
          } catch (error) {
-             debug_message("Localstorage can't be reached: " + error);
+             debugMessage("Localstorage can't be reached: " + error);
          }
          var markerLatLng = new google.maps.LatLng(locaties[i][3], locaties[i][4]);
          routeList.push(markerLatLng);
 
-         varArray.markerArray[i] = {};
+         config.markerArray[i] = {};
          for (var attr in locatieMarker) {
-             varArray.markerArray[i][attr] = locatieMarker[attr];
+             config.markerArray[i][attr] = locatieMarker[attr];
          }
          markerArray[i].scale = locaties[i][2] / 3;
 
          var marker = new google.maps.Marker({
              position: markerLatLng,
-             map: varArray.map,
-             icon: varArray.markerArray[i],
+             map: config.map,
+             icon: config.markerArray[i],
              title: locaties[i][0]
          });
        }
 
-       if(tourType == varArray.lineair){
+       if(tourType == config.lineair){
            // Draw lines between the points
-           debug_message("Drawing route");
+           debugMessage("Drawing route");
            var route = new google.maps.Polyline({
                clickable: false,
-               map: varArray.map,
+               map: config.map,
                path: routeList,
                strokeColor: 'Black',
                strokeOpacity: .6,
                strokeWeight: 3
            });
        }
-
        // Add the location of the user
-       varArray.currentPositionMarker = new google.maps.Marker({
+       config.currentPositionMarker = new google.maps.Marker({
            position: kaartOpties.center,
-           map: varArray.map,
+           map: config.map,
            icon: positieMarker,
            title: 'U are here'
        });
-
-       ET.addListener(varArray.positionUpdated, update_positie);
-
+       ET.addListener(config.positionUpdated, updatePositie);
      },
 
      isNumber: function(n) {
@@ -218,32 +196,28 @@
      },
 
      // Update the users position on the map
-     update_positie: function(event) {
+     updatePositie: function(event) {
        // use currentPosition to center the map
-       var newPos = new google.maps.LatLng(varArray.currentPosition.coords.latitude, varArray.currentPosition.coords.longitude);
-       varArray.map.setCenter(newPos);
-       varArray.currentPositionMarker.setPosition(newPos);
+       var newPos = new google.maps.LatLng(config.currentPosition.coords.latitude, config.currentPosition.coords.longitude);
+       config.map.setCenter(newPos);
+       config.currentPositionMarker.setPosition(newPos);
      }
-
    };
 
    var debugModule = {
-
-     _geo_error_handler: function(code, message) {
-       debug_message('geo.js error ' + code + ': ' + message);
+     geoErrorHandler: function(code, message) {
+       debugMessage('geo.js error ' + code + ': ' + message);
      },
 
-     debug_message: function(message) {
-       (varArray.customDebugging && varArray.debugId) ? document.getElementById(varArray.debugId).innerHTML : console.log(message);
+     debugMessage: function(message) {
+       (config.customDebugging && config.debugId) ? document.getElementById(config.debugId).innerHTML : console.log(message);
      },
 
-     set_custom_debugging: function(debugId) {
-       varArray.debugId = this.debugId;
-       varArray.customDebugging = true;
+     setCustomDebugging: function(debugId) {
+       config.debugId = this.debugId;
+       config.customDebugging = true;
      }
-
    };
-
   }()
 );
 
